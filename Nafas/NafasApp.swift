0a0efc68d8cc3ae19275b8fa2a2af5eed1db760d
Rana <rana.k.alngashy@gmail.com>
2026-03-15 00:36:03 +0300

    Initial Commit

---
+++ Nafas/NafasApp.swift
@@ -0,0 +1,17 @@
+//
+//  NafasApp.swift
+//  Nafas
+//
+//  Created by Rana Alngashy on 15/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct NafasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
